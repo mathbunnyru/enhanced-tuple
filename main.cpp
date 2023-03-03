8f6a355060f0c020877e6f0dfb5a680b1589aa10
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-03-03 15:13:42 +0400

    Minor fixes

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ struct S {
 
 template <template <typename... Types> class TupleType>
 void test_tuple(std::string_view description) {
-  std::cout << std::string(40, '=') << '\n' << description << '\n';
+  std::cout << std::string(40, '=') << "\ntesting: " << description << '\n';
 
   TupleType<int, double, S> simple_t(0, 1.5, S{20});
   std::get<2>(simple_t).num = 200;
@@ -27,9 +27,7 @@ void test_tuple(std::string_view description) {
 int main() {
   test_tuple<std::tuple>("std::tuple");
 
-  std::cout << "testing enh_inh::Tuple\n";
   test_tuple<enh_inh::Tuple>("enh_inh::Tuple");
 
-  std::cout << "testing enh_inh::Tuple\n";
   test_tuple<enh_rec::Tuple>("enh_inh::Tuple");
 }
